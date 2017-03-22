fdbb42a76ef4835225be0edc0354c83a081af3c8
jhollandUVic <jholland@uvic.ca>
2017-03-22 06:21:18 -0700

    Handles file with no colour
    Sends "nocolour.txt" through lines_to_svg.py. Expedient measure until lines_to_svg_colour.py available.

--- sierpinksi.sh
+++ sierpinksi.sh
@@ -2,28 +2,32 @@
 
 recursions=2
 scale_to_a_third=.3333
-
-#	Create a Recursive Sierpinski Carpet
+echo	"*"
+echo	"* Create a Recursive Sierpinski Carpet with ${recursions} Levels"
 CMD="python generate_carpet.py ${recursions} > carpet_${recursions}.txt"
 echo "${CMD}"
 eval "${CMD}"
-
-#	Create svg file
-# CMD="python lines_to_svg_colour.py < carpet_${recursions}.txt > carpet_${recursions}.svg"
+echo	"*"
+#	echo	"Create svg file"
+#	CMD="python lines_to_svg_colour.py < carpet_${recursions}.txt > carpet_${recursions}.svg"
 #	echo "${CMD}"
-# eval "${CMD}"
-
-#	Scale carpet lines down to fit in central third of canvas
+#	eval "${CMD}"
+echo	"*"
+echo	"* Scale carpet lines down to fit in central third of canvas"
 CMD="python rotate_scale_translate.py -f ${scale_to_a_third} < carpet_${recursions}.txt > carpet_${recursions}S.txt"
 echo "${CMD}"
 eval "${CMD}"
-
-# Build some tile patterns from scaled file
+echo	"*"
+echo	"* Build some tile patterns from scaled file"
 CMD="python transform_carpet.py < carpet_${recursions}S.txt > carpet_${recursions}ST.txt"
 echo "${CMD}"
 eval "${CMD}"
-
-# Create svg file
+echo	"*"
+echo	"Create black and white svg file"
+CMD="python lines_to_svg.py < nocolour.txt > carpet_${recursions}ST.svg"
+echo "${CMD}"
+eval "${CMD}"
+#	Create colour svg file
 #	CMD="python lines_to_svg_colour.py < carpet_${recursions}ST.txt > carpet_${recursions}ST.svg"
 #	echo "${CMD}"
-# eval "${CMD}"
+#	eval "${CMD}"
